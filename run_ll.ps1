94429599d20f233c914e1b2edfb8ed1b5a077ab9
Viola Martin <viola_martin@icloud.com>
2024-10-22 12:57:38 +0100

    Adding cdef statements to one_energy/get_order/initdata

--- run_ll.ps1
+++ run_ll.ps1
@@ -4,7 +4,7 @@ $sizes = @(10, 25, 45, 100, 210, 460, 1000, 2150, 4640, 10000)
 
 # Loop over each size and run the Python script
 foreach ($size in $sizes) {
-    $outputFile = "$outputDir\cython-raw.txt"
+    $outputFile = "$outputDir\cython-basic.txt"
     Write-Output "Running: python .\run_lebwhollasher.py 50 $size 0.5 0"
     $output = python .\run_lebwhollasher.py 50 $size 0.5 0 
     $output | Out-File -FilePath $outputFile -Append
